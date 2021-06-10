b3f98e4bd4d9f92b0cd4f6d34383e5f4c31fbe63
Filipe Moraes Mendes <10069692+fmmendes@users.noreply.github.com>
2021-06-10 11:19:06 -0300

    Update main.c

--- c/main/c00/ex07/main.c
+++ c/main/c00/ex07/main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
+#include <limits.h>
 
-void	ft_rev_int_tab(int *tab, int size);
+void	ft_putnbr(int nb);
 
 int main(void)
 {
-    printf("\e[1;34mExercício 07: \e[0m");
-
-	int arr[5] = {0, 1, 2, 3, 4};
-	int arr2[4] = {1, 2, 4, 3};
-
-	ft_rev_int_tab(arr, 5);
-	ft_rev_int_tab(arr2, 4);
-
-	if (!(arr[0] == 4 && arr[1] == 3 && arr[2] == 2 && arr[3] == 1 && arr[4] == 0))
-	{
-		printf("\e[1;31mKO, expected {4, 3, 2, 1, 0} got {%i, %i, %i, %i, %i}\e[0m", arr[0], arr[1], arr[2], arr[3], arr[4]);
-	}
-	else if (!(arr2[0] == 3 && arr2[1] == 4 && arr2[2] == 2 && arr2[3] == 1))
-		printf("\e[1;31mKO, expected {3, 4, 2, 1} got {%i, %i, %i, %i}\e[0m", arr2[0], arr[1], arr[2], arr[3]);
-	else
-		printf("\e[1;32mOK\e[0m");
-    return 0;
+	write(1, "-1: ", 4);
+	ft_putnbr(-1);
+	write(1, "\n1: ", 4);
+	ft_putnbr(1);
+	write(1, "\n-10: ", 6);
+	ft_putnbr(-10);
+	write(1, "\n10: ", 5);
+	ft_putnbr(10);
+	write(1, "\nINT_MAX: ", 10);
+	ft_putnbr(INT_MAX);
+	write(1, "\nINT_MIN: ", 10);
+	ft_putnbr(INT_MIN);
+	write(1, "\n", 1);
+	return (0);
 }
