f7148494d63403b041fba7b11ca697d1684aab06
fimoraes <fimoraes@student.42sp.org.br>
2021-06-16 04:39:36 +0000

    WIP

--- c/main/c00/ex07/main.c
+++ c/main/c00/ex07/main.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <limits.h>
+#include <unistd.h>
 
 void	ft_putnbr(int nb);
 
 int main(void)
 {
-	write(1, "-1: ", 4);
+	write(1, "INT_MIN: ", 10);
+	ft_putnbr(INT_MIN);
+	write(1, "\n-1: ", 5);
 	ft_putnbr(-1);
+	write(1, "\n0: ", 4);
+	ft_putnbr(0);
 	write(1, "\n1: ", 4);
 	ft_putnbr(1);
-	write(1, "\n-10: ", 6);
-	ft_putnbr(-10);
-	write(1, "\n10: ", 5);
-	ft_putnbr(10);
 	write(1, "\nINT_MAX: ", 10);
 	ft_putnbr(INT_MAX);
-	write(1, "\nINT_MIN: ", 10);
-	ft_putnbr(INT_MIN);
 	write(1, "\n", 1);
 	return (0);
 }
