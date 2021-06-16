635bc97c700363b511861addd708a9aad90fd0a7
fimoraes <fimoraes@student.42sp.org.br>
2021-06-16 15:01:48 +0000

    :smile:

--- c/main/c02/ex06/main.c
+++ c/main/c02/ex06/main.c
@@ -8,20 +8,14 @@ int		main(void)
 	char buffer0[200];
 
 	buffer0[1] = 0;
-	for(int i = 0; i <= 127; ++i)
+	for(int i = 1; i <= 127; ++i)
 	{
 		buffer0[0] = i;
 		if ((!!isprint(i)) != (!!ft_str_is_printable(buffer0)))
-			if (i != 0)
-				{
-					printf("KO char %i is %sprintable\n", i, isprint(i) ? "" : "not ");
-					return (0);
-				}
-				if (i != 1)
-				{
-					printf("KO char %i is %sprintable\n", i, isprint(i) ? "" : "not ");
-					return (0);
-				}
+		{
+			printf("KO char %i is %sprintable\n", i, isprint(i) ? "" : "not ");
+			return (0);
+		}
 	}
 
 	printf("OK");
